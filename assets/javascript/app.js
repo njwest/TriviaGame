58c0360e0351d06a71e11168b038990d57e3e9a9
Nick West <mail@nickwe.st>
2016-06-04 00:49:16 -0400

    added correct answer gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,10 @@
-var timer = 60;
+
+window.onload = function(){
+
+var exp = 0
 
 var questionOne ={
-	text:"Which jazz musician was infamous for stabbing Cab Calloway in the butt?",
+	text:"Who was infamous for stabbing Cab Calloway?",
 	answers:[
 				{text: "Dizzy Gillespie", correct: "true"},
 				{text: "Fats Navarro", correct: "false"},
@@ -20,19 +23,27 @@ var questionTwo={
 				],
 };
 
-$("#question").html("<p class='question'>" +  questionOne.text + "</p>");
+$("#question").html("<p>" +  questionOne.text + "</p>");
 
 for (var i = 0; i < questionOne.answers.length; i++){
 	$("#answers").append("<button class='" + questionOne.answers[i].correct + "'>" + questionOne.answers[i].text + "</button>");
 };
 
+$(".next").click(function(){
+	$("#game").html("asdfas");
+});
+
 $(".false").click(function(){
-	$("#game").html("<p>You wrong!</p>");
+	$("#game").html("<h2>Incorrect.</h2><button class='next'>Next Question </button>");
 });
 
 $(".true").click(function(){
-	$("#game").html("<p>You right...</p>");
+	$("#game").html("<div><img src='assets/images/hamsters.gif'/></div><h2>You right!</h2><button class='next'>Next Question </button>");
+	exp += 2000;
 });
+
+
+}
 
 //function answers(){
 //	if (false){
