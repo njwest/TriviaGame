ce2e0677ea037384862b4a4311099f67ae69a6aa
Nick West <mail@nickwe.st>
2016-06-05 09:25:48 -0400

    Rest of questions added, ' results' pre-page added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,18 +11,14 @@ var quizGame = {
 	time: 60,
 	start: function(){
         count = setInterval(quizGame.count, 1000);
+        
     },
    count: function(){
       quizGame.time--;
       var currentTime2 = quizGame.timeConverter(quizGame.time);
       $('#timer').html(currentTime2);
     },
-    stop: function(){
-        clearInterval(count);
-    },
-   countReset: function () {
-        
-    },
+   
    timeConverter: function(t){
         var minutes = Math.floor(t/60);
         var seconds = t - (minutes * 60);
@@ -38,8 +34,12 @@ var quizGame = {
         return minutes + ":" + seconds;
     },
 
-	question: ["Of the following Jazz musicians, who stabbed Cab Calloway?",
+	question: ["Who was infamous for stabbing Big Band leader Cab Calloway?",
 					"While serving as Vice President, Richard Nixon unknowingly smuggled a suitcase containing three pounds of cannabis through customs for this American jazz ambassador:",
+					"The best-selling jazz album of all time is:",
+					"Which of the following is not considered a style of jazz:",
+					"Who of the following jazz musicians was not born in New Jersey:",
+					"Who is playing the background music for this quiz game?"
 					],
 	answers: [
 					[
@@ -53,23 +53,59 @@ var quizGame = {
 						["Chet Baker", false],
 						["Louis Armstrong", true],
 						["Ella Fitzgerald", false]
+					],
+					[
+						["Dave Brubeck - Time Out", false],
+						["Miles Davis - Kind of Blue", true],
+						["Herbie Hancock - Head Hunters", false],
+						["John Coltrane - Blue Train", false]
+					],
+					[
+						["Modal", false],
+						["Hard bop", false],
+						["Diddle bop", true],
+						["Cool", false]
+					],
+					[
+						["Esperanza Spalding", true],
+						["Bill Evans", false],
+						["Joe Pass", false],
+						["Wayne Shorter", false]
+					],
+					[
+						["Art Tatum", false],
+						["Art Blakey", false],
+						["Horace Silver", false],
+						["Bud Powell", true]
 					]
-
 				],
 	questionGen: function () {
 		$("#answers").empty();
 		console.log(quizGame.question[q]);
 		quizGame.time = 60;
    	count = setInterval(quizGame.count, 1000);
+   	setTimeout(function(){
+        	$("#question").html("<h2>You're out of time...</h2>");
+        	$("#answers").html('<img width="50%" src="assets/images/hamsters.gif"/>');
+        	$("#button").html("<button id='start'>Next Question </button>");
+        	quizGame.time = 60;
+   		clearInterval(count);
+         $('#timer').html('00:60');
+        		}, 
+        	60000);
    	$("#button").empty();
    	$("#question").html("<div>" + quizGame.question[q] + "</div>");
    	for(var i = 0; i < quizGame.answers[q].length; i++){
 			$("#answers").append("<div><button class='" + quizGame.answers[q][i][1] + "'>" + quizGame.answers[q][i][0] + "</button></div>");
    	 };
    	$(".true").click(function(){
-   		$("#question").html("<h2>Correct!</h2>")
+   		$("#question").html("<h2>Correct!</h2>");
    		$("#answers").html("<img width='50%' src='assets/images/hamsters.gif'/>");
+   		if(q === quizGame.question.length){
+   			$("#question").html("<h2>Correct!</h2><button id='finish'>Results</button>")
+   		} else{
    		$("#button").html("<button id='start'>Next Question </button>");
+   		};
    		quizGame.time = 60;
    		clearInterval(count);
         $('#timer').html('00:60');
@@ -79,7 +115,11 @@ var quizGame = {
    	$(".false").click(function(){
    		$("#question").html("<h2>Incorrect!</h2>")
    		$("#answers").html("<img width='50%' src='assets/images/hamsters.gif'/>");
-			$("#button").html("<button id='start'>Next Question </button>");
+			if(q === quizGame.question.length){
+   			$("#question").html("<h2>Inorrect!</h2><button id='finish'>Results</button>")
+   		} else{
+   		$("#button").html("<button id='start'>Next Question </button>");
+   		};
 			quizGame.time = 60;
         	$('#timer').html('00:60');
         	clearInterval(count);
